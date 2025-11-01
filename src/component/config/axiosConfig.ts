02825af2edf02b21fb6331d83741241429297685
hoangnd <hoangndse184443@fpt.edu.vn>
2025-11-01 19:43:57 +0700

    fix

--- src/component/config/axiosConfig.ts
+++ src/component/config/axiosConfig.ts
@@ -1,10 +1,5 @@
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 
-const httpsBaseUrl = "https://localhost:7256/api";
-const httpBaseUrl = "http://localhost:5168/api";
-const envBaseUrl = import.meta.env.VITE_API_BASE_URL;
-
-// Tạo instance mặc định
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: { "Content-Type": "application/json" },
@@ -17,32 +12,7 @@ axiosInstance.interceptors.request.use((config) => {
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-
-  // Debug log
-  console.log("📡 Request:", {
-    url: config.url,
-    method: config.method,
-    headers: config.headers,
-    data: config.data,
-  });
-
   return config;
 });
-
-// ✅ Fallback HTTPS → HTTP
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  async (error: AxiosError) => {
-    if (
-      error.code === "ERR_NETWORK" &&
-      error.config?.baseURL === httpsBaseUrl
-    ) {
-      console.warn("⚠️ HTTPS không chạy, fallback sang HTTP");
-      error.config.baseURL = httpBaseUrl;
-      return axios.request(error.config);
-    }
-    return Promise.reject(error);
-  }
-);
 
 export default axiosInstance;
